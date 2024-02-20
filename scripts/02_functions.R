970424b874b5d4dcdf2176e8073edd25164bf60a
Ericbf-10 <ericbaufa10@gmail.com>
2024-02-20 15:36:42 +0100

    Finish FORA for all mutation lists (C5)

--- scripts/02_functions.R
+++ scripts/02_functions.R
@@ -3,3 +3,13 @@
 ########################
 
 ## Functions needed for the project
+
+### Set data.frame columns of type list to character
+set_lists_to_chars <- function(x) {
+  if(class(x) == 'list') {
+    y <- paste(unlist(x[1]), sep='', collapse=', ')
+  } else {
+    y <- x 
+  }
+  return(y)
+}
