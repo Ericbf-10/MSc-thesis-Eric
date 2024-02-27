c99e028838c43da1684e5b0264ba7ab2a229b5b0
Ericbf-10 <ericbaufa10@gmail.com>
2024-02-27 17:33:55 +0100

    Standardize fora analysis in function and produce more plots

--- scripts/02_functions.R
+++ scripts/02_functions.R
@@ -84,6 +84,56 @@ ENTREZtoENSEMBL <- function(x){
   return(vec)
 }
 
+# Perform FORA analysis for a specific Human Gene Set collection
+perform_fora_analysis <- function(category, subcategory, all_mut_list, nonsyn_mut_list, lof_mut_list, fora_results_all_csv_path, fora_results_nosnyn_csv_path, fora_results_lof_csv_path) {
+  # Load required libraries
+  library(msigdbr)
+  library(fgsea)
+  library(EnsDb.Hsapiens.v75)
+  
+  # Get a list of gene sets for the specified category with their associated genes
+  gene_sets_df <- msigdbr(species = "human", category = category, subcategory = subcategory)
+  gene_sets_list <- split(x = gene_sets_df$ensembl_gene, f = gene_sets_df$gs_name)
+  
+  # Get all the Ensembl human Gene IDs
+  genes_info <- genes(EnsDb.Hsapiens.v75)
+  ensembl_hgene_ids <- unique(genes_info$gene_id)
+  
+  # Initialize lists to store FORA results
+  fora_results_all_list <- list()
+  fora_results_nonsyn_list <- list()
+  fora_results_lof_list <- list()
+  
+  for(sample_id in sample_id_list) {
+    # Perform FORA for all mutations
+    mutated_genes_all_ids <- all_mut_list[[sample_id]]$ensembl_id
+    fora_results_all_list[[sample_id]] <- fora(gene_sets_list, mutated_genes_all_ids, ensembl_hgene_ids)
+    
+    # Perform FORA for nonsynonymous mutations
+    mutated_genes_nonsyn_ids <- nonsyn_mut_list[[sample_id]]$ensembl_id
+    fora_results_nonsyn_list[[sample_id]] <- fora(gene_sets_list, mutated_genes_nonsyn_ids, ensembl_hgene_ids)
+    
+    # Perform FORA for LoF mutations
+    mutated_genes_lof_ids <- lof_mut_list[[sample_id]]$ensembl_id
+    fora_results_lof_list[[sample_id]] <- fora(gene_sets_list, mutated_genes_lof_ids, ensembl_hgene_ids)
+  }
+  
+  # Save the results to CSV files using the provided variable names
+  fora_results_all_df <- merge_fora_data_tables_adding_id(fora_results_all_list)
+  write.csv(fora_results_all_df, file = fora_results_all_csv_path, row.names = TRUE)
+  
+  fora_results_nonsyn_df <- merge_fora_data_tables_adding_id(fora_results_nonsyn_list)
+  write.csv(fora_results_nonsyn_df, file = fora_results_nosnyn_csv_path, row.names = TRUE)
+  
+  fora_results_lof_df <- merge_fora_data_tables_adding_id(fora_results_lof_list)
+  write.csv(fora_results_lof_df, file = fora_results_lof_csv_path, row.names = TRUE)
+  
+  # Return
+  return(list(fora_results_all_list = fora_results_all_list,
+              fora_results_nonsyn_list = fora_results_nonsyn_list,
+              fora_results_lof_list = fora_results_lof_list))
+}
+
 # Plot top 5 Gene Sets across samples stratified by response
 plot_top5 <- function(response_df, mut_list, plot_path, fig_title, fig_y_label) {
   
@@ -152,4 +202,74 @@ plot_top5 <- function(response_df, mut_list, plot_path, fig_title, fig_y_label) 
   
   # Return
   return(top5_plot)
+}
+
+# Plot (a) specific Gene Set(s) across samples stratified by response
+plot_geneset <- function(geneset, response_df, mut_list, plot_path, fig_title, fig_y_label) {
+  # geneset is a vector i.e. c("GS1", "GS2")
+  
+  # Prepare data for all samples in the list: Filter Gene Set & calculate Gene ratio per sample from a mut_list
+  prepared_data_list <- lapply(names(mut_list), function(sample_id) {
+    mut_list[[sample_id]] %>%
+      dplyr::filter(pathway %in% geneset) %>% 
+      dplyr::mutate(Gene_ratio = overlap / size,
+                    Count = overlap,
+                    sample_id = sample_id) %>%
+      dplyr::arrange(desc(Gene_ratio))
+  })
+  
+  # Combine all prepared datasets into one
+  combined_data <- bind_rows(prepared_data_list)
+  
+  # Merge response info
+  combined_data <- combined_data %>%
+    dplyr::left_join(response_df, by = "sample_id")
+  
+  # Step 1: Create an ordered factor for sample_id based on patient_response
+  # First, create a mapping of sample_id to patient_response
+  response_order <- combined_data %>%
+    dplyr::select(sample_id, patient_response) %>%
+    dplyr::distinct() %>%
+    dplyr::arrange(patient_response, sample_id) # Arrange so that "R" comes before "NR"
+  
+  # Use this ordering to reorder sample_id in the combined data
+  combined_data$sample_id <- factor(combined_data$sample_id,
+                                    levels = response_order$sample_id)
+  
+  # Determine dynamic breaks based on quantiles of Gene Ratio
+  gene_ratio_breaks <- quantile(combined_data$Gene_ratio, probs = seq(0, 1, by = 0.5), na.rm = TRUE)
+  
+  # Step 2: Plot with ordered sample_id and facets to distinguish "R" and "NR"
+  geneset_plot <- ggplot(combined_data, aes(x = sample_id, 
+                                                    y = reorder(pathway, Gene_ratio),
+                                                    size = Gene_ratio,
+                                                    color = padj)) +
+    geom_point() +
+    scale_color_gradient(low = "blue", high = "red", trans = "log10",
+                         limits = c(1e-6, 1), oob = scales::oob_squish) +
+    scale_size_continuous(name = "Gene Ratio",
+                          range = c(1, 6),  # Adjust size range to match your preference
+                          breaks = gene_ratio_breaks,  # Define breaks based on "Gene Ratio" distribution
+                          labels = format(gene_ratio_breaks, digits = 1)) +  # Label breaks as needed
+    facet_grid(. ~ patient_response, scales = "free_x", space = "free_x") +
+    theme_minimal() +
+    theme(
+      panel.background = element_rect(fill = "white", colour = "black"),
+      panel.grid.major = element_line(color = "grey", linewidth = 0.5),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "white", colour = NA),
+      legend.position = "right",
+      axis.text.x = element_text(angle = 45, hjust = 1)
+    ) +
+    labs(title = fig_title,
+         x = "Sample ID",
+         y = fig_y_label,
+         color = "p.adjust (log10)") +
+    guides(size = guide_legend(title = "Gene Ratio"))
+  
+  # Save plot
+  ggsave(plot_path, geneset_plot, width = 12, height = 8, dpi = 300)
+  
+  # Return
+  return(geneset_plot)
 }
